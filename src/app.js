cf00b0fa7cc019f4a708f52911567739ef9d5f1a
J11an <jaysun1w@gmail.com>
2023-01-29 15:58:12 -0800

    react basics

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Bob", animal: "Dog", breed: "Sausage" }),
+    React.createElement(Pet, { name: "Dog", animal: "Dog", breed: "Dog" }),
+    React.createElement(Pet, { name: "Cat", animal: "Cat", breed: "Kitty" }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
